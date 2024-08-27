af6f15310d242d65c731da78c7aaca9a5cc04a2f
Dynia <piotr.dynia@gmail.com>
2024-08-27 14:22:13 +0200

    budget renewal worker created

--- app/src/main/java/com/example/spendee/SpendeeApp.kt
+++ app/src/main/java/com/example/spendee/SpendeeApp.kt
@@ -1,7 +1,32 @@
 package com.example.spendee
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.spendee.workmanager.BudgetRenewalWorker
 import dagger.hilt.android.HiltAndroidApp
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
 @HiltAndroidApp
-class SpendeeApp: Application()
+class SpendeeApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val workRequest = PeriodicWorkRequestBuilder<BudgetRenewalWorker>(5, TimeUnit.SECONDS)
+            .build()
+
+        WorkManager.getInstance(this).enqueue(workRequest)
+    }
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
