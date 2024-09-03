263ac84cafbfed0e99102556d929dbfc3de560c4
Dynia <piotr.dynia@gmail.com>
2024-09-03 15:47:04 +0200

    first integration budget test done (finally)

--- app/src/androidTest/java/com/example/spendee/feature_budget/presentation/budget/BudgetScreenTest.kt
+++ app/src/androidTest/java/com/example/spendee/feature_budget/presentation/budget/BudgetScreenTest.kt
@@ -1,18 +1,28 @@
 package com.example.spendee.feature_budget.presentation.budget
 
 import SpendeeTheme
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.spendee.MainActivity
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.spendee.R
+import com.example.spendee.core.presentation.util.LoadingScreen
 import com.example.spendee.core.presentation.util.Routes
+import com.example.spendee.core.presentation.util.UiEvent
 import com.example.spendee.di.AppModule
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
+import kotlinx.coroutines.flow.MutableStateFlow
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.whenever
 
 @HiltAndroidTest
 @UninstallModules(AppModule::class)
@@ -22,11 +32,23 @@ class BudgetScreenTest {
     val hiltRule = HiltAndroidRule(this)
 
     @get:Rule(order = 1)
-    val composeRule = createAndroidComposeRule<MainActivity>()
+    val composeRule = createComposeRule()
+
+    private lateinit var mockViewModel: BudgetViewModel
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
 
     @Before
     fun setUp() {
         hiltRule.inject()
+
+        val mockUiEvent = mock(UiEvent::class.java)
+
+        mockViewModel = mock(BudgetViewModel::class.java).apply {
+            whenever(budget).thenReturn(MutableStateFlow(null))
+            whenever(isLoading).thenReturn(MutableStateFlow(false))
+            whenever(uiEvent).thenReturn(MutableStateFlow(mockUiEvent))
+        }
+
         composeRule.setContent {
             val navController = rememberNavController()
             SpendeeTheme {
@@ -35,10 +57,34 @@ class BudgetScreenTest {
                     startDestination = Routes.BUDGET
                 ) {
                     composable(route = Routes.BUDGET) {
-//                        BudgetScreen(budget = , onEvent = , uiEvent = , onNavigate = )
+                        val viewModel = mockViewModel
+                        val budget = viewModel.budget.collectAsStateWithLifecycle().value
+                        val isLoading = viewModel.isLoading.collectAsStateWithLifecycle().value
+
+                        when {
+                            isLoading -> LoadingScreen()
+                            budget == null -> NoBudgetScreen(
+                                onEvent = viewModel::onEvent,
+                                onNavigate = { navController.navigate(it) },
+                                uiEvent = viewModel.uiEvent
+                            )
+                            else -> BudgetScreen(
+                                budget = budget,
+                                onEvent = viewModel::onEvent,
+                                onNavigate = { navController.navigate(it) },
+                                uiEvent = viewModel.uiEvent
+                            )
+                        }
+
                     }
                 }
             }
         }
+    }
+
+    @Test
+    fun showNoBudgetScreenWhenNoBudget() {
+        composeRule.onNodeWithText(context.getString(R.string.you_have_no_budget_set)).assertIsDisplayed()
+        composeRule.onNodeWithText(context.getString(R.string.set_budget)).assertIsDisplayed()
     }
 }
