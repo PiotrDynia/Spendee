619900f4e6c0628eee4d3ba6746e6a07e35bf411
Dynia <piotr.dynia@gmail.com>
2024-09-03 17:30:32 +0200

    budget integration tests in progress

--- app/src/androidTest/java/com/example/spendee/feature_budget/presentation/budget/BudgetScreenTest.kt
+++ app/src/androidTest/java/com/example/spendee/feature_budget/presentation/budget/BudgetScreenTest.kt
@@ -1,9 +1,13 @@
 package com.example.spendee.feature_budget.presentation.budget
 
 import SpendeeTheme
+import androidx.compose.ui.test.SemanticsNodeInteractionCollection
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -14,6 +18,7 @@ import com.example.spendee.core.presentation.util.LoadingScreen
 import com.example.spendee.core.presentation.util.Routes
 import com.example.spendee.core.presentation.util.UiEvent
 import com.example.spendee.di.AppModule
+import com.example.spendee.feature_budget.domain.model.Budget
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
@@ -23,6 +28,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.kotlin.whenever
+import java.time.LocalDate
 
 @HiltAndroidTest
 @UninstallModules(AppModule::class)
@@ -44,7 +50,19 @@ class BudgetScreenTest {
         val mockUiEvent = mock(UiEvent::class.java)
 
         mockViewModel = mock(BudgetViewModel::class.java).apply {
-            whenever(budget).thenReturn(MutableStateFlow(null))
+            whenever(budget).thenReturn(
+                MutableStateFlow(
+                    Budget(
+                        totalAmount = 200.0,
+                        leftToSpend = 180.0,
+                        totalSpent = 20.0,
+                        startDate = LocalDate.now().minusDays(5),
+                        endDate = LocalDate.now().plusDays(5),
+                        isExceeded = false,
+                        isExceedNotificationEnabled = true
+                        )
+                )
+            )
             whenever(isLoading).thenReturn(MutableStateFlow(false))
             whenever(uiEvent).thenReturn(MutableStateFlow(mockUiEvent))
         }
@@ -68,6 +86,7 @@ class BudgetScreenTest {
                                 onNavigate = { navController.navigate(it) },
                                 uiEvent = viewModel.uiEvent
                             )
+
                             else -> BudgetScreen(
                                 budget = budget,
                                 onEvent = viewModel::onEvent,
@@ -84,7 +103,61 @@ class BudgetScreenTest {
 
     @Test
     fun showNoBudgetScreenWhenNoBudget() {
-        composeRule.onNodeWithText(context.getString(R.string.you_have_no_budget_set)).assertIsDisplayed()
+        whenever(mockViewModel.budget).thenReturn(MutableStateFlow(null))
+        composeRule.setContent {
+            val navController = rememberNavController()
+            SpendeeTheme {
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.BUDGET
+                ) {
+                    composable(route = Routes.BUDGET) {
+                        val viewModel = mockViewModel
+                        val budget = viewModel.budget.collectAsStateWithLifecycle().value
+                        val isLoading = viewModel.isLoading.collectAsStateWithLifecycle().value
+
+                        when {
+                            isLoading -> LoadingScreen()
+                            budget == null -> NoBudgetScreen(
+                                onEvent = viewModel::onEvent,
+                                onNavigate = { navController.navigate(it) },
+                                uiEvent = viewModel.uiEvent
+                            )
+
+                            else -> BudgetScreen(
+                                budget = budget,
+                                onEvent = viewModel::onEvent,
+                                onNavigate = { navController.navigate(it) },
+                                uiEvent = viewModel.uiEvent
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        composeRule.onNodeWithText(context.getString(R.string.you_have_no_budget_set))
+            .assertIsDisplayed()
         composeRule.onNodeWithText(context.getString(R.string.set_budget)).assertIsDisplayed()
+    }
+
+    @Test
+    fun showBudgetScreenWhenBudgetIsPresent() {
+        composeRule.onAllNodesWithText(context.getString(R.string.you_can_spend)).assertAreDisplayed()
+        composeRule.onAllNodesWithText(context.getString(R.string.spent)).assertAreDisplayed()
+    }
+
+    @Test fun showOptionsMenuOnClick() {
+        composeRule
+            .onNodeWithContentDescription(context.getString(R.string.more_options))
+            .performClick()
+        composeRule.onNodeWithText(context.getString(R.string.edit)).assertIsDisplayed()
+        composeRule.onNodeWithText(context.getString(R.string.delete)).assertIsDisplayed()
+    }
+
+    private fun SemanticsNodeInteractionCollection.assertAreDisplayed(): SemanticsNodeInteractionCollection {
+        fetchSemanticsNodes().forEachIndexed { index, _ ->
+            get(index).assertIsDisplayed()
+        }
+        return this
     }
 }
